b8cf7cdd73517f34b65ed1ef9349aee4a936394f
Montso Moalosi <montsoernestmoalosi@gmail.com>
2023-12-12 03:02:01 +0200

    AddContact Form Component JSX

--- src/components/AddContact.js
+++ src/components/AddContact.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
-const AddContact = () => {
-  return (
-    <div>AddContact</div>
-  )
+class AddContact extends React.Component {
+  render(){
+    return(
+        <div className='ui main'>
+            <h2>Add Contact</h2>
+            <from className="ui form">
+                <div className='field'>
+                    <label>Name</label>
+                    <input type='text' name='name' placeholder='Name'/>
+                </div>
+                <div className='contact'>
+                    <label>Contact</label>
+                    <input type='number' name='contact' placeholder='Contact'/>
+                </div>
+                <div className='email'>
+                    <label>Email</label>
+                    <input type='email' name='email' placeholder='Email'/>
+                </div>
+                <button className='ui button blue'>Add</button>
+            </from>
+        </div>
+    )
+  }
 }
 
 export default AddContact
